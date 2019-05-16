f57ed770ec980f72531eb4b6baa9161ea9a3c350
hfridland <hfridland@shaw.ca>
2019-05-16 12:50:38 -0700

    Programming Completed 1

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -10,9 +10,9 @@ const about = () => (
             <img src={avatar} alt="It's me" />
         </div>
         <p>Hi, my name is <strong>Haim Fridland</strong>. I'm programmer with ~35 years experience. 
-        My resume can be found at <a href='https://docs.google.com/document/d/e/2PACX-1vSBx20QsUgwKpE_EOaNbH_xBEToBlCfgdoIU7zQSiOVEShBnBXyK6liTP84hZK403jFXtm7LDcTh8pO/pub' target='_blank'> 
+        My resume can be found at <a href='https://docs.google.com/document/d/e/2PACX-1vSBx20QsUgwKpE_EOaNbH_xBEToBlCfgdoIU7zQSiOVEShBnBXyK6liTP84hZK403jFXtm7LDcTh8pO/pub' target='_blank' rel='noopener noreferrer'> 
         https://docs.google.com/document/d/e/2PACX-1vSBx20QsUgwKpE_EOaNbH_xBEToBlCfgdoIU7zQSiOVEShBnBXyK6liTP84hZK403jFXtm7LDcTh8pO/pub</a>.
-        You see my React portfolio. Sources can be found at <a href='https://github.com/hfridland/pizza' target='_blank'>https://github.com/hfridland/pizza</a> I'm looking for a job or orders. 
+        You see my React portfolio. Sources can be found at <a href='https://github.com/hfridland/pizza' target='_blank' rel='noopener noreferrer'>https://github.com/hfridland/pizza</a> I'm looking for a job or orders. 
          My hourly rate is $35CAD/hour negotiable. I live in Vancouver, BC, Canada. You can contact me by email <a href='mailto:hfridland@shaw.ca'>hfridland@shaw.ca</a>.</p>
     </div>
 );
